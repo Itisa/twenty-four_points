00c3542a721d49b263124d72b02a24c14df176ad
Itisa <shshr2005@163.com>
2017-11-20 19:22:45 +0800

    first find 1.1

--- find1.py
+++ find1.py
@@ -2,15 +2,28 @@ import time
 
 def find_number(nums,f):
 	i = 0
-	if f in nums:
-		i = nums.index(f)
-	else:
-		i = -1
+	a = nums
+	print a.index(f)
+	b = 0
+	c = 0
+	d = 0
+	for z in range(22):
+	# while len(a) != 1:
+		b = int(len(a) / 2 )
+		c = a[0]
+		if not c == 1:
+			d = 2
+		else:
+			d = 3
+		if a[(b + 2) * d / 2 ] >= f:
+			a = range(c,b * 3 / 2 + c)
+		else:
+			a = range(b + c,len(a) + c)
+		print c,len(a)	
+	return a[0],a
 
-	return i
-
-ds = range(1,10000)
-f = 123
+ds = range(1,10001)
+f = 100
 t = time.time()
 findex = find_number(ds,f)
 print 'cost time:',time.time() - t
