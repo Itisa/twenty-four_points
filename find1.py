c1df14d9b06e956e9ab4b6cee152516d30d50e6a
Itisa <shshr2005@163.com>
2017-11-26 20:10:15 +0800

    first find 1.4

--- find1.py
+++ find1.py
@@ -1,27 +1,50 @@
 import time
+import random
+
+def get_data():
+	xs = range(59)
+	ds = random.sample(xs,20)
+	ds.sort()
+	return ds
 
 def find_number(nums,f):
-	a = nums
+	b = nums
+	a = range(0,len(b))
 	middle = 0
-	while len(a) != 2:
-		middle = (a[0] + a[len(a) - 1]) / 2
-		if f == a[len(a) -1]:
-			return a[len(a) - 2]
-		if f > a[len(a) - 1] or f < a[0]:
-			return 'The nummiddleer is out of range.'
-		if middle > f:
-			a = range(a[0],middle + 1)
-		else:
-			a = range(middle,a[len(a) - 1] + 1)
-		print a[0],len(a)
-	print middle,a 
-	return a[0] - 1
 
-ds = range(1,20001)
+	while len(a) != 1:	
+	# for z in range(10):
+		# print a,
+
+		middle = len(a) / 2
+		# print middle
+
+		if b[a[middle]] == f:
+			return a[middle]
+		if b[middle + a[0]] > f:
+			a = range(a[0],a[middle])
+		else:
+			a = range(a[middle],a[-1] + 1)
+
+	if b[a[0]] == f:
+		return a[0]
+	else:
+		return -1
+
+# ds = range(1,20001)
+ds = get_data()
 # ds = [x * 2 for x in ds]
-f = 20000
+f = ds[len(ds) / 3]
+f = ds[-3] + 1
+print 'find ',f
 t = time.time()
 findex = find_number(ds,f)
 print 'cost time:',time.time() - t
 
-print findex
+print ds
+
+print 'result:',findex
+print 'answer:',ds[findex]
+if ds[findex] == f:
+	print 'yes'
+# print ds[findex],f
