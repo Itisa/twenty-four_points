3707723804848807c75066559d6c891c7bfb7c54
Itisa <shshr2005@163.com>
2017-11-27 19:24:48 +0800

    first find 1.5(1)

--- find1.py
+++ find1.py
@@ -2,41 +2,49 @@ import time
 import random
 
 def get_data():
-	xs = range(59)
-	ds = random.sample(xs,20)
+	xs = range(500)
+	ds = random.sample(xs,251)
 	ds.sort()
 	return ds
 
 def find_number(nums,f):
 	b = nums
 	a = range(0,len(b))
+	c = 0
+	d = 1
 	middle = 0
 
-	while len(a) != 1:	
-	# for z in range(10):
+	while (d - c) != 0:
+	# for z in range(30):
 		# print a,
 
-		middle = len(a) / 2
-		# print middle
+		middle = (c + d) / 2
+		# print middle,
 
 		if b[a[middle]] == f:
 			return a[middle]
 		if b[middle + a[0]] > f:
-			a = range(a[0],a[middle])
+			c = a[0 + c]
+			d = a[middle - 1 - c]
 		else:
-			a = range(a[middle],a[-1] + 1)
+			c = a[middle + c]
+			d = a[a[-1] / 2]
+		# print range(c,d + 1)
+		# print c,d
 
-	if b[a[0]] == f:
-		return a[0]
+	print b[c+2]
+	if b[c+2] == f:
+		return c+2
 	else:
 		return -1
 
 # ds = range(1,20001)
 ds = get_data()
 # ds = [x * 2 for x in ds]
-f = ds[len(ds) / 3]
-f = ds[-3] + 1
-print 'find ',f
+f = ds[len(ds) / 5]
+# f = ds[0]
+print 'find:',f
+print 'index:',ds.index(f)
 t = time.time()
 findex = find_number(ds,f)
 print 'cost time:',time.time() - t
