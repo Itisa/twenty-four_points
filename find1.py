c7d60ce75f1e8c9a03446fb1f28e50926443994d
Itisa <shshr2005@163.com>
2017-11-28 19:49:36 +0800

    first find (done)

--- find1.py
+++ find1.py
@@ -1,58 +1,75 @@
 import time
 import random
 
+class task():
+	def __init__(self,x,y,name):
+		self.x = x
+		self.y = y
+		self.name = name
+
+def get_obj():
+	ss = range(64)
+	tasks = []
+	for i in ss:
+		x = task(i,i*2,"song")
+		tasks.append(x)
+
+	return tasks
+
+def get_string():
+	s = 'abcdefghijklmnopqrstuvwxyz'
+	ss = [x for x in s]
+	return ss[:16]
+
 def get_data():
-	xs = range(500)
-	ds = random.sample(xs,251)
+	xs = range(64)
+	ds = random.sample(xs,32)
 	ds.sort()
 	return ds
 
-def find_number(nums,f):
-	b = nums
-	a = range(0,len(b))
-	c = 0
-	d = 1
+def find_number(n,f):
+	a = range(0,len(n))
+	start = 0
+	end = len(n) - 1
 	middle = 0
 
-	while (d - c) != 0:
-	# for z in range(30):
-		# print a,
+	while 1:
 
-		middle = (c + d) / 2
-		# print middle,
+		middle = (start + end) / 2
 
-		if b[a[middle]] == f:
-			return a[middle]
-		if b[middle + a[0]] > f:
-			c = a[0 + c]
-			d = a[middle - 1 - c]
+		if n[middle] == f:
+			return middle
 		else:
-			c = a[middle + c]
-			d = a[a[-1] / 2]
-		# print range(c,d + 1)
-		# print c,d
+			if start >= end:
+				return -1
 
-	print b[c+2]
-	if b[c+2] == f:
-		return c+2
+		if n[middle] > f:
+			end = middle - 1
+		else:
+			start = middle + 1
+
+	if n[start+2] == f:
+		return start+2
 	else:
 		return -1
 
 # ds = range(1,20001)
-ds = get_data()
-# ds = [x * 2 for x in ds]
-f = ds[len(ds) / 5]
-# f = ds[0]
+# ds = get_data()
+ds = get_string()
+# print get_obj()
+# ds = get_obj()
+
+f = 'x'
 print 'find:',f
-print 'index:',ds.index(f)
+if f in ds:
+	print 'index:',ds.index(f)
+else:
+	print 'index:number is not in the list'
 t = time.time()
 findex = find_number(ds,f)
+
 print 'cost time:',time.time() - t
-
-print ds
-
 print 'result:',findex
 print 'answer:',ds[findex]
 if ds[findex] == f:
 	print 'yes'
-# print ds[findex],f
