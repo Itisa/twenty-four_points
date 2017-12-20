54eff4514b0e086042e4e92befe25dd550e673b2
Itisa <shshr2005@163.com>
2017-12-20 19:31:23 +0800

    a change(1)

--- all changes.py
+++ all changes.py
@@ -1,23 +1,19 @@
+import math
 
 def get_changes(n):
-	a = range(1,n+1)
-	b = a[:]
-	c = a[:]
-	d = 0
-	x = []
-	for z in b:
-		b = c[:]
-		for z1 in b:
-			
-			for z2 in b:
-				x.append(z2)
-			print x
-			b.insert(b[z1-1],b[2])
-			b.pop(2)
-			x = []
-		c.append(c[0])
-		c.pop(0)
+	a = 0
+	b = 0
+	x = range(1,n+1)
+	y = []
+	for z in x:
+		y.append(int(math.pow(n,z)))
+	y.reverse()
+	print y
 
+	for z in range(20):
+		x[0] = z/y[0]
+		x[1] = z/y[1]
+		x[2] = z/y[2]
+		print z,':',x
 
-
-get_changes(4)
+get_changes(3)
