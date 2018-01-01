dbf34398c7189ab2b2f178cc0c95e582956ea723
Itisa <shshr2005@163.com>
2018-01-01 20:20:58 +0800

    change a hex

--- all changes.py
+++ all changes.py
@@ -3,27 +3,28 @@ import math
 def get_changes(n):
 	a = 0
 	b = 0
-	x = range(1,n+1)
+	x = [''] * n
 	y = []
+	f = 0
+	z = 0
+	re = []
+
 	for z in range(n):
 		y.append(int(math.pow(n,z)))
 	y = [t * n for t in y]
 	y.reverse()
-	# y = [9,3,27]
-	print y
+
 	z = 0
-	# 1,9,27
-	for z in range(95):
-		x = [0,0,0]
+	for z in range(y[0]):
+		zs = z*n
+		f = 0
+
 		for z1 in range(n):
-			x[z1] = (z/y[z1])%(y[z1])
-		x[-1] = z%(y[-1])-y[0]*x[0]-y[1]*x[1]
-		# x[0] = (z/9)%3
-		# x[1] = (z/3)%9-3*x[0]	
-		# x[2] = z%(27)-3*x[1]-9*x[0]
-		print z,':',x
-		if x[0] == n-1 and x[1] == n-1 and x[2] == n-1:
-			break
-		# z += 1
-get_changes(4)
+			x[z1] = ((zs-f)/y[z1])%(y[z1])
+			f += x[z1] * y[z1]
+		re.append(x[:])
+		
+	return re
+
+print get_changes(3)
  
