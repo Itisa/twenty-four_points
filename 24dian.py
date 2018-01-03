be4ae8bc1a5c7de94cc2a673ebeae2e3a9bc8d4a
Itisa <shshr2005@163.com>
2018-01-03 20:45:53 +0800

    24dian(2)

--- 24dian.py
+++ 24dian.py
@@ -1,12 +1,16 @@
 import math
-from string import maketrans
 
 def x(m,n,o,p):
+	m = float(m)
+	n = float(n)
+	o = float(o)
+	p = float(p)
 	x = get_changes()
 	e = 0
+
 	while 1:
 		e += 1
-		if e > 1:
+		if e > 24:
 			return 'nothing'
 			break
 		if e == 1:
@@ -21,70 +25,36 @@ def x(m,n,o,p):
 		 	z = [m,p,n,o]
 		if e == 21:
 			z = [m,o,n,p]	
+
 		z.append(z[0])
 		z.pop(0)
 		a1 = z[0]
 		b1 = z[1]
 		c1 = z[2]
 		d1 = z[3]
-		a = 0
 		
-		for z1 in range(1):
-			u = '0123'
-			i = '+-*/'
-			r = maketrans(u,i)
+		for z1 in range(64):
+			fu0 = x[z1][0]
+			fu1 = x[z1][1]
+			fu2 = x[z1][2]
 
-			fu1 = x[z1][0]
-			fu1 = '%d' % fu1
-			print fu1
-			# fu1.translate(r)
+			ans1 = get_signs(fu0,a1,b1)
+			ans2 = get_signs(fu1,ans1,c1)
+			ans3 = get_signs(fu2,ans2,d1)
+			if ans3 == 24:
+				return a1,fu0,b1,fu1,c1,fu2,d1
 
-			if fu1 == '+':
-				ans1 = a1 + b1
-
-			# if a >= 1 and a <= 16:
-			# 	ans1 = a1 + b1
-			# 	fu1 = '+'
-			# elif a >= 17 and a <= 32:
-			# 	ans1 = a1 - b1
-			# 	fu1 = '-'
-			# elif a >= 33 and a <= 48:
-			# 	ans1 = a1 * b1
-			# 	fu1 = '*'
-			# else:
-			# 	ans1 = a1 / b1
-			# 	fu1 = '/'
 			
-			# if a%16 >= 1 and a%16 <=4:
-			# 	ans2 = ans1 + c1
-			# 	fu2 = '+'
-			# elif a%16 >= 5 and a%16 <=8:
-			# 	ans2 = ans1 - c1
-			# 	fu2 = '-'
-			# elif a%16 >= 9 and a%16 <=12:
-			# 	ans2 = ans1 * c1
-			# 	fu2 = '*'
-			# else:
-			# 	ans2 = ans1 / c1
-			# 	fu2 = '/'
-
-			# if a%4 == 1:
-			# 	ans3 = ans2 + d1
-			# 	fu3 = '+'
-			# elif a%4 == 2:
-			# 	ans3 = ans2 - d1
-			# 	fu3 = '-'
-			# elif a%4 == 3:
-			# 	ans3 = ans2 * d1
-			# 	fu3 = '*'
-			# else:
-			# 	ans3 = ans2 / d1
-			# 	fu3 = '/'
-			
-			# if ans3 == 24:
-			# 	y = [a1,fu1,b1,fu2,c1,fu3,d1]
-			# 	return y
-			# return fu1,fu2,fu3
+def get_signs(s,a,b):
+	if s == 0:
+		ans = a + b
+	elif s == 1:
+		ans = a - b
+	elif s == 2:
+		ans = a * b
+	else:
+		ans = a / b
+	return ans
 
 def get_changes():
 	x = [''] * 4
@@ -100,6 +70,17 @@ def get_changes():
 		re.append(x[1:])
 	return re
 
+def final(lis):
+	if lis == 'nothing':
+		return 'nothing'
+	a = [0,1,2,3]
+	b = ['+','-','*','/']
+	c = []
+	for z in lis:
+		c.append(z)
+	for z in range(3):
+		c[z*2+1] = b[a.index(c[z*2+1])]
+	return c
 
-print get_changes()
-print x(10,10,4,4)
+an = x(5,1,5,5)
+print final(an)
