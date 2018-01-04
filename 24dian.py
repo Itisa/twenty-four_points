4941cd15c0d4345b298ff2d1743b7b1f402a9c59
Itisa <shshr2005@163.com>
2018-01-04 20:00:26 +0800

    24dian(3)

--- 24dian.py
+++ 24dian.py
@@ -1,49 +1,33 @@
 import math
 
+SCORE = 24
+
 def x(m,n,o,p):
-	m = float(m)
-	n = float(n)
-	o = float(o)
-	p = float(p)
-	x = get_changes()
-	e = 0
+	nums = [m,n,o,p]
+	signs = get_changes()
+	chas = get_chas()
+	ls = len(signs)
+	lenc = len(chas)
 
-	while 1:
-		e += 1
-		if e > 24:
-			return 'nothing'
-			break
-		if e == 1:
-			z = [m,n,o,p]
-		if e == 5:
-			z = [m,n,p,o]
-		if e == 9:
-			z = [n,m,o,p]
-		if e == 13:
-			z = [n,m,p,o]
-		if e == 17:
-		 	z = [m,p,n,o]
-		if e == 21:
-			z = [m,o,n,p]	
+	for z in range(lenc):
+		a1 = nums[chas[z][0]]
+		b1 = nums[chas[z][1]]
+		c1 = nums[chas[z][2]]
+		d1 = nums[chas[z][3]]		
 
-		z.append(z[0])
-		z.pop(0)
-		a1 = z[0]
-		b1 = z[1]
-		c1 = z[2]
-		d1 = z[3]
-		
-		for z1 in range(64):
-			fu0 = x[z1][0]
-			fu1 = x[z1][1]
-			fu2 = x[z1][2]
+		for z1 in range(ls):
+			fu0 = signs[z1][0]
+			fu1 = signs[z1][1]
+			fu2 = signs[z1][2]
 
 			ans1 = get_signs(fu0,a1,b1)
 			ans2 = get_signs(fu1,ans1,c1)
 			ans3 = get_signs(fu2,ans2,d1)
-			if ans3 == 24:
+			
+			if ans3 == SCORE:
 				return a1,fu0,b1,fu1,c1,fu2,d1
-
+	
+	return 'nothing'
 			
 def get_signs(s,a,b):
 	if s == 0:
@@ -61,13 +45,29 @@ def get_changes():
 	y = [256,64,16,4]
 	re = []
 	z = 0
-	for z in range(y[0]):
+	for z in range(256):
 		zs = z*4
 		f = 0
 		for z1 in range(4):
 			x[z1] = ((zs-f)/y[z1])%(y[z1])
 			f += x[z1] * y[z1]
 		re.append(x[1:])
+	return re
+
+
+def get_chas():
+	x = [''] * 4
+	y = [256,64,16,4]
+	re = []
+	z = 0
+	for z in range(256):
+		zs = z*4
+		f = 0
+		for z1 in range(4):
+			x[z1] = ((zs-f)/y[z1])%(y[z1])
+			f += x[z1] * y[z1]
+		if x.count(1) == 1 and x.count(2) == 1 and x.count(3) == 1:
+			re.append(x[:])
 	return re
 
 def final(lis):
@@ -80,7 +80,9 @@ def final(lis):
 		c.append(z)
 	for z in range(3):
 		c[z*2+1] = b[a.index(c[z*2+1])]
-	return c
 
-an = x(5,1,5,5)
+	return c
+	# return c[0],c[1],c[2],c[3],c[4],c[5],c[6]
+
+an = x(10,10,4,4)
 print final(an)
