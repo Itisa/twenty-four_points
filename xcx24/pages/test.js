1b3a17b1b72e7c50e5482f323efa49f7d825ba6a
Itisa <shshr2005@163.com>
2018-02-05 21:13:43 +0800

    24dian_xcx(1)

--- xcx24/pages/test.js
+++ xcx24/pages/test.js
@@ -1,143 +1,17 @@
 // pages/test.js
-var types = ['default','primay','warn'],anss,q1,q2,q3,nums="",alnums=[]
-var pageObject = {
-  data: {
-    plain: false,
-    loading: false
-  },
-  setDisabled: function () {
-    this.setData({
-      disabled: !this.data.disabled
-    })
-  },
-  setPlain: function () {
-    this.setData({
-      plain: !this.data.plain
-    })
-  },
-  setLoading: function () {
-    this.setData({
-      loading: !this.data.loading
-    })
-  },
-  // 第二个
-  Count: function (e) {
-    console.log(q1,q2,q3);
-    if(!q1 || !q2 || !q3){
-      this.setData({ ans: "没有给全参数"})
-      console.log(1)
-    }else{
-      this.setData({ ans: q1-q2-q3 });
-      console.log(2)
-    }
-  },
-  Count1: function (e) {
-    q1 = parseInt(e.detail.value);
-  },
-  Count2: function (e) {
-    q2 = parseInt(e.detail.value);
-  },
-  Count3: function (e) {
-    q3 = parseInt(e.detail.value);
-  },
-  //第三个
-
-  AC: function (e) {
-    nums = "";
-    alnums = [];
-    this.setData({ ans: nums});
-    console.log("AC",nums)
-  },
-  Numm: function (e) {
-    nums += e.target.dataset.num;
-    this.setData({ ans: nums });
-    // console.log(nums,e.target.dataset.num);
-  },
-  Add: function (e) {
-    alnums.push(parseFloat(nums));
-    console.log(alnums);
-    if(alnums.length==3){
-      console.log(123);
-      switch(alnums[1]){
-        case "+":
-          alnums = [alnums[0]+alnums[2]];
-          break;
-        case "-":
-          alnums = [alnums[0]-alnums[2]];
-          break;
-        case "*":
-          alnums = [alnums[0]*alnums[2]];
-          break;
-        case "/":
-          if (alnums[2] == 0) {
-            nums = "错误";
-            this.setData({ ans: nums });           
-            break;
-          }
-          alnums = [alnums[0]/alnums[2]];
-          break;        
-      }
-    }
-    console.log(alnums);
-    alnums.push(e.target.dataset.fu);
-    console.log(alnums);
-    nums = "";
-    this.setData({ ans: nums });
-  },
-  Equal: function (e) {
-    alnums.push(parseFloat(nums));
-    switch (alnums[1]) {
-      case "+":
-        alnums = [alnums[0] + alnums[2]];
-        nums = alnums[0];
-        break;
-      case "-":
-        alnums = [alnums[0] - alnums[2]];
-        nums = alnums[0];
-        break;
-      case "*":
-        alnums = [alnums[0] * alnums[2]];
-        nums = alnums[0];
-        break;
-      case "/":
-        if(alnums[2] == 0){
-          nums = ["错误"];
-          this.setData({ ans: nums});
-          break;
-        }
-        alnums = [alnums[0] / alnums[2]];
-        nums = alnums[0];
-        break;
-    }
-    console.log(alnums)
-    alnums.pop()
-    if(nums != "错误"){
-      this.setData({ ans: nums });
-    }
-  },
-  Del: function (e) {
-    nums = nums.slice(0,-1);
-    this.setData({ ans: nums });
-    console.log(nums.slice(0,-1),nums);
+Page({
+  get_ans: function (e) {
+    console.log(1)
+    this.setData({ ans: 123 })
   }
-
-} 
-
-
-Page(pageObject)
-
-function Count () {
   
-} 
+})
 
-
-
-// Page({
 //   /**
 //    * 页面的初始数据
 //    */
 //   data: {
-
+  
 //   },
 
 //   /**
@@ -196,4 +70,3 @@ function Count () {
   
 //   }
 // })
-
